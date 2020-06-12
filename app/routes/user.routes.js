4af31717d8d4cfc658ac4d6387daac57ba08d073
Aleksey <virtyoz777@gmail.com>
2020-06-12 15:26:12 +0900

    сделал схему для дб

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -5,7 +5,7 @@ const router = Router();
 const User = new UserController();
 
 //NOTE/:  Получить пользователя по id
-router.post("/:id", User.show);
+router.get("/:id", User.show);
 
 //NOTE/: Удалить пользователя по id
 router.delete("/:id", User.delete);
